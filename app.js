91bbfa02d39e0c258c6fedb226544a841ec9eace
hamidr-source <hr454837@gmail.com>
2024-03-15 12:30:38 -0700

    Add data in page for each cities

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const inputElem = document.querySelector(".search-box");
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+const weather = document.querySelector(".weather");
+const highLow = document.querySelector(".hi-low");
 
 const apiData = {
   url: "https://api.openweathermap.org/data/2.5/weather?q=",
@@ -7,10 +11,18 @@ const apiData = {
 
 const fetchData = () => {
   let countryName = inputElem.value;
-    console.log(countryName)
-  fetch(`${apiData.url}${countryName}&appid=${apiData.key}`).then((res) =>
-    console.log(res)
-  );
+
+  fetch(`${apiData.url}${countryName}&appid=${apiData.key}`)
+    .then((res) => res.json())
+    .then((data) => showData(data));
+};
+
+const showData = (data) => {
+  console.log(data);
+  city.innerHTML = `${data.name}, ${data.sys.country}`;
+  temp.innerHTML = `${Math.round(data.main.temp - 273.15)}°c`;
+  weather.innerHTML = `${data.weather[0].main}`;
+  highLow.innerHTML = `${Math.round(data.main.temp_max - 273.15)}°c / ${Math.round(data.main.temp_min - 273.15)}°c`
 };
 
 inputElem.addEventListener("keypress", (event) => {
